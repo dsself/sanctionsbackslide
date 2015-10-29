feaf7dc48b321f7382d2eae27dcbfdcc8392d6f1
dsself <dsself@wm.edu>
2015-10-29 12:05:35 -0400

    Finalize MI - Start Presentation

--- Spectrum/merge.R
+++ Spectrum/merge.R
@@ -1,10 +1,12 @@
 setwd("C:/Users/darin/Documents/sanctionsbackslide/Spectrum")
 library(haven)
 library(dplyr)
+library(readr)
 library(standard)
 library(ggplot2)
 library(scales)
 
+#Load, join, and write csv####
 load("marinov_ldpaper.RData")
 dfm <-x
 load("sanctionsrepdata.RData")
@@ -35,7 +37,7 @@ df <- right_join(dfw, dfm, by = c("Year", "CCode")) %>%
   left_join(dfs, by = c("Year", "CCode")) %>% 
   filter(!is.na(CCode)) %>%
   group_by(CCode) %>%
-  mutate(dpolity = polity2 - lag(polity2))  %>%
+  mutate(dpolity = as.vector(polity2 - lag(polity2)))  %>%
   mutate(ds = lag(S)- lag(lag(S))) %>%
   mutate(dw = lag(W) - lag(lag(W))) %>%
   mutate(ndpolity = as.numeric(ifelse(dpolity < 0,1,0)))  %>%
@@ -43,26 +45,65 @@ df <- right_join(dfw, dfm, by = c("Year", "CCode")) %>%
   select(Year, CCode, sanctions, sanctionsl1, polity2, dpolity, S, W, ndpolity, ds, dw, lagus, lagun, lagpopgled, laggdpchng, industry1, GDP_UN, energy2, pop1, 
          logpop, urban01, MEast, demaut) %>% 
   mutate(lGDP_UN = log(GDP_UN)) %>% 
-  mutate(dpolityb = as.numeric(ifelse(dpolity != 0, 1, 0)))
-
-write_csv(df, "SanctionsFinal.csv")
-
-df <- df %>% 
-  group_by(Year, polity2) %>% 
-  mutate(sum = sum(dpolityb)) %>% 
-  select(CCode, Year, polity2, dpolityb, sum) %>% 
-  group_by(polity2) 
+  mutate(dpolityb = as.numeric(ifelse(dpolity != 0, 1, 0))) 
 
 dp <- df %>% 
   group_by(Year, polity2) %>% 
   filter(!is.na(dpolityb)) %>% 
   count(dpolityb, polity2) %>% 
   group_by(polity2) %>% 
-  mutate(P = n/sum(n)) %>% 
-  filter(dpolityb == 1)
+  mutate(Pdpolity = n/sum(n)) %>% 
+  filter(dpolityb == 1) %>% 
+  select(polity2, Pdpolity)
 
-ggplot(dp, aes(x=polity2, y = P)) +
-  geom_bar(stat = "identity") +
-  ggtitle("Probability of Change in Polity")+
-  scale_y_continuous(labels = percent)
+df <- merge(df, dp, by = "polity2", all = T) %>% 
+  arrange(CCode, Year) %>% 
+  filter(!is.na(polity2))
 
+write_csv(df, "SanctionsMerged.csv")
+
+#Multiple Imputations####
+df <- read_csv("SanctionsMerged.csv") %>% 
+  filter(!is.na(polity2)) 
+df <- df[-2466,]
+
+dfe <- select(df, Year, CCode, energy2)
+dfi <- select(df, Year, CCode, industry1)
+dfu <- select(df, Year, CCode, urban01)
+
+dfe$energy2[dfe$energy2 == "-Inf"] <- NA
+
+library(Amelia)
+library(matrixStats)
+
+energy <- amelia(x = dfe, m = 15, ts = "Year", cs = "CCode", polytime = 1)
+
+de <- do.call(cbind,lapply(energy$imputations,function(x) x[x$Year > 1970,])) %>% 
+  select(imp1.Year, imp1.CCode, contains("energy2")) %>% 
+  mutate(menergy = rowMeans(.[,3:17])) %>% 
+  mutate(senergy = rowSds(as.matrix(.[,3:17]))) %>% 
+  select(Year = imp1.Year, CCode = imp1.CCode, menergy, senergy)
+
+industry <- amelia(x = dfi, m = 15, ts = "Year", cs = "CCode", polytime = 1)
+
+di <- do.call(cbind,lapply(industry$imputations,function(x) x[x$Year > 1970,])) %>% 
+  select(imp1.Year, imp1.CCode, contains("industry1")) %>% 
+  mutate(mindustry = rowMeans(.[,3:17])) %>% 
+  mutate(sindustry = rowSds(as.matrix(.[,3:17]))) %>% 
+  select(Year = imp1.Year, CCode = imp1.CCode, mindustry, sindustry)
+
+urban <- amelia(x = dfu, m = 15, ts = "Year", cs = "CCode", polytime = 1)
+
+du <- do.call(cbind,lapply(urban$imputations,function(x) x[x$Year > 1970,])) %>% 
+  select(imp1.Year, imp1.CCode, contains("urban01")) %>% 
+  mutate(murban = rowMeans(.[,3:17])) %>% 
+  mutate(surban = rowSds(as.matrix(.[,3:17]))) %>% 
+  select(Year = imp1.Year, CCode = imp1.CCode, murban, surban)
+
+df <- inner_join(df, de, by = c("Year", "CCode")) %>% 
+  inner_join(di, by = c("Year", "CCode")) %>% 
+  inner_join(du, by = c("Year", "CCode")) %>% 
+  select(-urban01, -energy2, -industry1) 
+
+write_csv(df, "SanctionsFinal.csv")
+  
